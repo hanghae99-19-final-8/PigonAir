f28c25e53c65fc79bbf7fadd5cc6a2503235ee35
gbk1414 <gbk1414@gmail.com>
2024-04-04 13:45:07 +0900

    fix: FlightDataGenerator Test로 이식 완료 PigonAirApplication 주석 제거

--- src/main/java/com/example/pigonair/PigonAirApplication.java
+++ src/main/java/com/example/pigonair/PigonAirApplication.java
@@ -1,11 +1,8 @@
 package com.example.pigonair;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.example.pigonair.domain.flight.service.FlightDataGenerator;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -17,16 +14,6 @@ import io.swagger.v3.oas.annotations.servers.Server;
 public class PigonAirApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PigonAirApplication.class, args);
-		// // 항공편 난수 생성 (100개)
-		// ConfigurableApplicationContext context =SpringApplication.run(PigonAirApplication.class, args);
-		// FlightDataGenerator flightDataGenerator = context.getBean(FlightDataGenerator.class);
-		//
-		// // Generate random flight data for 100 flights
-		// int numberOfFlights = 100;
-		// flightDataGenerator.generateRandomFlightData(numberOfFlights);
-		//
-		// // Close the application context
-		// context.close();
 	}
 	//
 	// @Bean
