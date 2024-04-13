4d5c354b943a05406c52a00af5cab902cbcce94c
gbk1414 <gbk1414@gmail.com>
2024-04-13 19:34:37 +0900

    feat(Payment.java): Indexing 구현
    @Index annotation 사용, 조회하는데 사용되는, reservation_id만 인덱싱

--- src/main/java/com/example/pigonair/domain/payment/entity/Payment.java
+++ src/main/java/com/example/pigonair/domain/payment/entity/Payment.java
@@ -11,7 +11,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,12 +23,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "payment", indexes = {@Index(columnList = "reservation_id")})
 public class Payment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@OneToOne
+	@JoinColumn(name = "reservation_id")
 	private Reservation reservation;
 
 	private String serialNumber;
