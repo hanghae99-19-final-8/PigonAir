21a596d1c1b87d64697858393654d2a348100f05
gbk1414 <gbk1414@gmail.com>
2024-04-13 19:35:00 +0900

    feat(Reservation.java): Indexing 구현
    @Index annotation 사용, 조회하는데 사용되는, member_id만 인덱싱

--- src/main/java/com/example/pigonair/domain/reservation/entity/Reservation.java
+++ src/main/java/com/example/pigonair/domain/reservation/entity/Reservation.java
@@ -10,8 +10,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,12 +23,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "reservation", indexes = {@Index(columnList = "member_id")})
 public class Reservation {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne // 추후에 @ManyToOne
+	@ManyToOne
+	@JoinColumn(name = "member_id")
 	private Member member;
 
 	@OneToOne
